9ddf69a89cc70850b6ad13121ec582e6e25c3d31
Juan Elías Galeote Cazorla <91273047+JuanGaleote@users.noreply.github.com>
2022-07-21 15:52:34 +0200

    Delete entities directory

--- entities/takeAction.m
+++
@@ -1,7 +0,0 @@
-function takeAction(mTimer,~)
-import java.awt.*;
-import java.awt.event.*;
-rob=Robot;
-rob.keyPress(KeyEvent.VK_CONTROL);
-rob.keyRelease(KeyEvent.VK_CONTROL);
-end
--- entities/timerStart.m
+++
@@ -1,3 +0,0 @@
-function timerStart(mTimer,~)
-disp('Start Timer.')
-end
